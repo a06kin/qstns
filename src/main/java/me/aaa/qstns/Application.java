c06b72bc9d4c2ee51690ef79aa2627209ca08fc3
Aleksandr Anohin <a06kin@gmail.com>
2014-12-12 11:48:42 +0200

    standalone&nonStandalone app

--- src/main/java/me/aaa/qstns/Application.java
+++ src/main/java/me/aaa/qstns/Application.java
@@ -1,9 +1,23 @@
 package me.aaa.qstns;
 
-import static spark.Spark.*;
-
 public class Application {
-    public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
-    }
 }
+
+/*
+Config for web.xml on server
+
+<filter>
+    <filter-name>SparkFilter</filter-name>
+    <filter-class>spark.servlet.SparkFilter</filter-class>
+    <init-param>
+        <param-name>applicationClass</param-name>
+        <param-value>com.company.YourApplication</param-value>
+    </init-param>
+</filter>
+
+<filter-mapping>
+    <filter-name>SparkFilter</filter-name>
+    <url-pattern>/*</url-pattern>
+</filter-mapping>
+
+ */
