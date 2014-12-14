3cd506225b12606c3e1d022c1df0d52b2e84e768
Aleksandr Anohin <a06kin@gmail.com>
2014-12-14 12:57:52 +0200

    Use jpa repository

--- src/main/java/me/aaa/qstns/service/qstn/QstnRepository.java
+++ src/main/java/me/aaa/qstns/service/qstn/QstnRepository.java
@@ -1,14 +1,11 @@
 package me.aaa.qstns.service.qstn;
 
 import me.aaa.qstns.domain.Qstn;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-/**
- * Created by aaa on 12/12/14.
- */
-public interface QstnRepository extends CrudRepository<Qstn, Long> {
+public interface QstnRepository extends JpaRepository<Qstn, Long> {
     List<Qstn> findByCountry(String country);
     List<Qstn> findAll();
 }
