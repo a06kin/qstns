30425fe8b8bc7519b2c69dd479fe9224adb8180d
Aleksandr Anohin <a06kin@gmail.com>
2014-12-14 12:56:46 +0200

    ip resolver implementation

--- src/main/java/me/aaa/qstns/service/cntr/CountryServiceImpl.java
+++ src/main/java/me/aaa/qstns/service/cntr/CountryServiceImpl.java
@@ -1,13 +1,47 @@
 package me.aaa.qstns.service.cntr;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
 
 @Service
 @Transactional
 public class CountryServiceImpl implements CountryService {
+
+    private final static String LOCALHOST = "127.0.0.1";
+    private final static String NIC = "92.240.66.50";
+
     @Override
     public String getCountryForClient(String ip) {
-        return "lv"; //TODO: implement http://www.telize.com service
+        String url = "http://www.telize.com/geoip/" + ( LOCALHOST.equals(ip) ? NIC : ip );
+
+        HttpClient client = HttpClientBuilder.create().build();
+
+        HttpGet request = new HttpGet(url);
+        request.addHeader("accept", "application/json");
+
+        String result;
+        HttpResponse response;
+        JSONObject json = null;
+
+        try {
+            response = client.execute(request);
+            result = EntityUtils.toString(response.getEntity());
+
+            json = new JSONObject(result);
+
+            return json.getString("country_code");
+
+        } catch (IOException | JSONException e) {
+            return "lv";
+        }
     }
 }
