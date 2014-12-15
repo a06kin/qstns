a1553af3ab8b05249b1c793d1c3f056e25952eba
Aleksandr Anohin <a06kin@gmail.com>
2014-12-15 19:31:18 +0200

    some new queries

--- src/main/java/me/aaa/qstns/service/qstn/QstnRepository.java
+++ src/main/java/me/aaa/qstns/service/qstn/QstnRepository.java
@@ -3,11 +3,19 @@ package me.aaa.qstns.service.qstn;
 import me.aaa.qstns.basis.enums.QstnStatus;
 import me.aaa.qstns.domain.Qstn;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.sql.Time;
 import java.util.List;
 
 public interface QstnRepository extends JpaRepository<Qstn, Long> {
-    List<Qstn> findByCountry(String country);
+    List<Qstn> findByCountryAndStatus(String country, QstnStatus status);
+
     List<Qstn> findAll();
+
     List<Qstn> findByStatus(QstnStatus status);
+
+    @Query("select q from Qstn q where q.time >= :from and q.country = :country")
+    List<Qstn> findByTimerame(@Param("from") Time from, @Param("country") String country);
 }
