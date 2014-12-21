e4f96a95f58ac0cd5a1e121d51f4047ae20a69bf
Aleksandr Anohin <a06kin@gmail.com>
2014-12-21 12:46:26 +0200

    new test (now 62% line coverage))))))

---
+++ src/test/java/me/aaa/qstns/service/cntr/CountryServiceImplTest.java
@@ -0,0 +1,41 @@
+package me.aaa.qstns.service.cntr;
+
+import me.aaa.qstns.basis.settings.QstnSettings;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CountryServiceImplTest {
+
+    private static final String NORMAL_IP = "8.8.8.8";
+    private static final String BAD_IP = "9.9.9.9.9.9";
+    private static final String COUNTRY = "lv";
+
+    @Mock
+    private QstnSettings qstnSettings;
+
+    @InjectMocks
+    private CountryServiceImpl countryService;
+
+    @Test
+    public void testGetCountryForClientNormal() throws IOException {
+        String country = "US";
+        String actualCountry = countryService.getCountryForClient(NORMAL_IP);
+        assertEquals(country, actualCountry);
+    }
+
+    @Test()
+    public void testGetCountryForClientBad() throws IOException {
+        when(qstnSettings.getDefaultCountry()).thenReturn(COUNTRY);
+        String actualCountry = countryService.getCountryForClient(BAD_IP);
+        assertEquals(COUNTRY, actualCountry);
+    }
+}
