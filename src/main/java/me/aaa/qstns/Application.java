c3769a2893ddb7261b67fc7c102c2ccbe2fd25bd
Aleksandr Anohin <a06kin@gmail.com>
2014-12-21 13:38:30 +0200

    clean code

--- src/main/java/me/aaa/qstns/Application.java
+++ src/main/java/me/aaa/qstns/Application.java
@@ -10,6 +10,10 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 public class Application {
 
+    private Application(){
+
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
